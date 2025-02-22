dda6cfe619570eba8dc0d26c4d730ea2d9c7b804
Favour Anefu <favouranefu@gmail.com>
2025-02-22 14:12:51 +0100

    implemented basic support for full posts extraction and convertion to markdown

--- src/scraper.py
+++ src/scraper.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
+from markdownify import markdownify as md
 
 def scrape(url, func):
     response = requests.get(url)
@@ -15,10 +16,27 @@ def scrape(url, func):
 def get_posts_archive(url: str, soup: BeautifulSoup):
     archive = soup.select('div.content p a:first-child')
 
-    return list(map(lambda post: (post.text, urljoin(url, post.attrs['href'])), archive))
+    return map(lambda post: (post.text, urljoin(url, post.attrs['href'])), archive)
 
+def parse_post(url: str, soup: BeautifulSoup):
+    post_content = soup.select('div.content')[0]
+
+    # remove comments section
+    post_content.select('div#comments_body')[0].decompose()
+
+    # remove the script tags from the post
+    for s in post_content.find_all('script'):
+        s.decompose()
+
+    return md(str(post_content))
 
 if __name__ == "__main__":
+    print("getting posts...")
     posts = scrape("http://www.aaronsw.com/weblog/fullarchive", get_posts_archive)
+    posts = filter(lambda p: p is not None, posts)
 
-    print(posts[0])
+    print("fetching and parsing post contents...")
+    # expand posts with their markdown contents
+    posts = list(map(lambda p: p + (scrape(p[1], parse_post),), posts))
+
+    print(posts[56][2])
