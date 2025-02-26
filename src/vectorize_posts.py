c30e2e7144020226298184dd615fc42639dd184b
Favour Anefu <favouranefu@gmail.com>
2025-02-26 11:03:34 +0200

    implemented basic three-sentence sliding context window

--- src/vectorize_posts.py
+++ src/vectorize_posts.py
@@ -2,11 +2,29 @@ import pickle
 import sys
 import nltk
 
-nltk.download('punkt')
-nltk.download('punkt_tab')
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt')
+    nltk.download('punkt_tab')
 
 def split_text_into_sentences(text: str):
-    return nltk.sent_tokenize(text)
+    # remove the last two sentences which are the call to follow on Twitter
+    # and the date of publication
+    return list(map(lambda s: s.strip(), nltk.sent_tokenize(text)[:-2]))
+
+# gives order to chunked sentences as well as attaches the locations of the
+# sentences before and after
+def contexify(sentences: list[str]):
+    contexified = []
+    for idx, s in enumerate(sentences):
+        contexified.append(dict(value=s,
+                                pos=idx,
+                                before=idx-1,
+                                after= -1 if idx == len(sentences) - 1 else idx + 1))
+
+    return contexified
+
 
 if __name__ == "__main__":
     if len(sys.argv) <= 1:
@@ -16,7 +34,6 @@ if __name__ == "__main__":
     with open(sys.argv[1], "rb") as f:
         posts = pickle.load(f)
 
-    for s in split_text_into_sentences(posts[0][2]):
-        print(s)
-        print()
-        print()
+    cnt = contexify(split_text_into_sentences(posts[75][2]))
+
+    print(cnt[1])
